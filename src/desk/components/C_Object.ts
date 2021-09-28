043296e40f168418831766be3d1e9cb211e3f5f2
e-totsu <kamilsohlich@gmail.com>
2021-09-28 02:49:13 +0200

    #1 28.09.2021

---
+++ src/desk/components/C_Object.ts
@@ -0,0 +1,8 @@
+class C_Object {
+  constructor() {
+    
+  }
+  show() {
+    console.log(true);
+  }
+}
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+  pages: {
+    index: {
+      entry: 'src/main.js'
+    }
+  }
+}
