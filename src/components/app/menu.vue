a114c8f6601e9cfec420c7f1b152fc5aacce66d7
Kamil Sohlich <kamilsohlich@gmail.com>
2021-10-23 15:24:42 +0200

    #1 23.10.2021

---
+++ src/components/app/menu.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <v-list
+      flat
+      color="transparent"
+    >
+      <v-list-item-group
+        class="d-inline-flex"
+      >
+        <v-list-item
+          v-for="item in menu_items"
+          :key="item.title"
+        >
+          <v-list-item-title>
+            {{ $t(item.title) }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+    menu_items: [
+      {
+        title: 'File',
+        options: []
+      },
+      {
+        title: 'Edit',
+        options: []
+      },
+      {
+        title: 'Layer',
+        options: []
+      },
+      {
+        title: 'Window',
+        options: []
+      },
+      {
+        title: 'Market',
+        options: []
+      },
+      {
+        title: 'Help',
+        options: []
+      },
+    ],
+  }),
+  methods: {
+  },
+}
+</script>
